9935977aef299be10c8dc9d45425d1e20fc7002f
drewxa <bo.ro.da@mail.ru>
2017-03-22 15:20:01 +0300

    boolean function tests

--- 2017/lab5/bf/boolean_function.h
+++ 2017/lab5/bf/boolean_function.h
@@ -14,7 +14,7 @@ public:
 
 	using iterator = typename std::vector<value_type>::iterator;
 	using const_iterator = typename std::vector<value_type>::const_iterator;
-	
+
 public:
 
 	// возвращает функцию которая равна xn с размерностью dimension
@@ -27,35 +27,36 @@ public:
 	// dimension = 4
 	// вернется функция "0011001100110011"
 	static boolean_function var(size_t n, size_t dimension);
-	
+
 	// тождественный ноль "от dimension переменных"
 	static boolean_function zero(size_t dimension);
-	
+
 	// тождественная единица "от dimension переменных"
 	static boolean_function one(size_t dimension);
-	
+
 	static boolean_function from_anf(std::vector<value_type> v);
-	
+
 	boolean_function();
-	
+
 	// задает фунцию тождественного нуля от n переменных
 	boolean_function(size_t n);
-	
+
 	// задает фунцию от n переменных. значение равно value
 	// пример
 	// пусть value = 14, т.е. 0...00001110 в двоичной системе
 	// а n = 3
 	// тогда АНФ boolean_function будет равна x + y + xy + zx + zy + zyx
 	boolean_function(unsigned long long value, size_type n);
-	
+
 	// пусть table = "01110000"
 	// тогда АНФ boolean_function будет равна x + y + xy + zx + zy + zyx
 	boolean_function(const std::string& table);
-	
+
 	// пусть table = {true, false, false, true};
 	// тогда АНФ boolean_function будет равна x + y + 1
 	boolean_function(const std::vector<value_type>& table);
-	
+	boolean_function(const std::initializer_list<bool> vars);
+
 	boolean_function(const boolean_function& table);
 
 	boolean_function& operator=(const boolean_function& rhs);
@@ -78,7 +79,7 @@ public:
 	// true если функции равны
 	//  иначе false
 	bool operator == (const boolean_function&) const;
-	
+
 	// true если одна функция сравнима и больше или равна rhs
 	// false если сравнима и меньше rhs
 	// logic_error если фунции не сравнимы
@@ -106,34 +107,39 @@ public:
 	// пусть boolean_function задает функцию f(x, y, z)
 	// тогда функция вернет f(vars[0], vars[1], vars[2])
 	bool operator()(const std::vector<bool>& vars) const;
+	bool operator()(const std::initializer_list<bool> vars) const;
 
 
 	// T(x1, x2, ..., xN) - текущая функция
 	// operator вернет новую функцию, которая равна композиции G = T(fs[0], fs[1], ..., fs[N-1])
 	boolean_function operator()(const std::vector<boolean_function>& fs) const;
-	
+	boolean_function operator()(const std::initializer_list<boolean_function> vars) const;
+
 	bool is_monotone() const;
 	bool is_symmetric() const;
 	bool is_linear() const;
 	bool is_T1() const;
 	bool is_T0() const;
 	bool is_balanced() const; //равновесная ли
+	size_t weight() const;
 
 	bool is_functionally_complete_system() const;
-	
+
 	// Возвращает АНФ функции
 	std::vector<value_type> anf() const;
 };
 
 // пусть boolean_function представляет из себя функцию "01110000"
 // тогда get_polynom вернет строку "x0 + x1 + x0x1 + x0x2 + x1x2 + x0x1x2"
-std::string get_polynom(const boolean_function&) const;
+std::string get_polynom(const boolean_function&);
 
-boolean_function opeartor + (const boolean_function& a, const boolean_function& b);
+boolean_function operator + (const boolean_function& a, const boolean_function& b);
 
-boolean_function opeartor * (const boolean_function& a, const boolean_function& b);
+boolean_function operator * (const boolean_function& a, const boolean_function& b);
 
-boolean_function opeartor | (const boolean_function& a, const boolean_function& b);
+boolean_function operator | (const boolean_function& a, const boolean_function& b);
+
+bool operator != (const boolean_function& a, const boolean_function& b);
 
 
 #endif // __BOOLEAN_FUNCTION_INCLUDED__
