36b7a7e1a048f07259ceec307566e705fbdb792c
drewxa <bo.ro.da@mail.ru>
2017-04-05 16:35:49 +0300

    functional object

---
+++ 2017/lab6/01.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include <string>
+#include <vector>
+
+// functional object
+template <typename T>
+struct CommonAddition
+{
+	T operator()(const T& a, const T& b)
+	{
+		return a + b;
+	}
+};
+
+// functional object
+template <typename T>
+struct CommonXOR
+{
+	T operator()(const T& a, const T& b)
+	{
+		return a ^ b;
+	}
+};
+
+// BinaryOp - custom function
+template <typename IT, typename T, typename BinaryOp>
+T accumulate(IT beg, IT end, T init, BinaryOp op)
+{
+	for (IT it = beg; it != end; ++it)
+	{
+		init = op(init, *it);
+	}
+	return init;
+}
+
+int main()
+{
+	std::vector<int> vec = {0, 1, 2, 3, 4, 5, 6, 7};
+
+	
+	std::cout << accumulate(vec.begin(), vec.end(), 0, CommonAddition<int>{}) << std::endl;
+
+
+	CommonXOR<int> obj;
+	std::cout << accumulate(vec.begin(), vec.end(), 0, obj) << std::endl;
+}
